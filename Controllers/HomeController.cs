f29e8acdc1a06c8f33578961a6ea3a9698b16f99
Burmau Garba <burmaud1@gmail.com>
2023-12-28 19:15:17 -0500

    init

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PartyInvites.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public ViewResult RsvpForm()
+    {
+        return View("RsvpForm");
+    }
+    public ViewResult AttendeeList()
+    {
+        return View();
+    }
+}
